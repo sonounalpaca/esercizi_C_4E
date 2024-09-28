e3b12a9015c3d4e6ac257014ab6e9673d41175f1
La_Ele <150442625+sonounalpaca@users.noreply.github.com>
2024-09-28 15:23:07 +0200

    Add files via upload

--- iterazioni/es_4.c
+++ iterazioni/es_4.c
@@ -1,31 +1,27 @@
 /*
-Dati due numeri interi positivi N1 e N2 con N2>N1, generare e
-visualizzare in ordine crescente i numeri interi compresi
-nell'intervallo chiuso [N1,N2]. 
+Dato N un numero intero positivo, generare e visualizzare in ordine crescente i numeri pari
+minori o uguali a N.
 */
 
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    int n1, n2, numero;
-    do
-    {
-        printf ("Inserisci un primo numero: ");
-        scanf ("%d", &n);
-        printf ("Inserisci un secondo numero: ");
-        scanf ("%d", &n);
-
-    } while (n1 >= n2);
-
-    numero = n1;
-    do
-    {
-        printf ("%d", numero);
-        numero = numero + 1;
-       
-    } while (numero <= n2);
     
+    int n, numero = 0;
+    do
+    {
+        printf ("Inserisci un numero: ");
+        scanf ("%d", &n);
+
+    } while (n <= 0);
+
+    do
+    {
+        printf ("%d ", numero);
+        numero = numero + 2;
+
+    } while (numero <= n);
 
     return 0;
 }
